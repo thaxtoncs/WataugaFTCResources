21793947c2700c499c1f5c624726b9f6dcb4814f
MJackBarnes <31902951+MJackBarnes@users.noreply.github.com>
2018-01-19 19:45:11 -0500

    Update and rename HoloX.java to HoloXTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HoloXTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HoloXTeleOp.java
@@ -11,26 +11,35 @@ import com.qualcomm.robotcore.util.Range;
 
 public class BasicOpMode_Linear extends OpMode {
     HWholoX robot = new HWholoX();
-    ModernRoboticsI2cCompassSensor compass;
-    compass = hardwareMap.get(ModernRoboticsI2cCompassSensor.class, "compass");
-    int Lx = null;
-    int Ly = null;
-    int angle = null;
-    int positivePower = null;
-    int negativePower = null;
+    // left joystick y-value
+    int Lx;
+    // left joystick x-value
+    int Ly;
+    // left trigger value
+    int lTrig;
+    // angle of the left joystick 
+    int angle;
+    // power of motors BL and FR
+    int positivePower;
+    // power of motors BR and FL
+    int negativePower;
+    // degrees to fix the forward direction
+    int directionFix = 45;
 }
     public void loop(){
+        // set the variables for I/O
+        lTrig = Range.clip(robot.gamepad1.right_trigger, 0, 1);
         Lx = Range.clip(robot.gamepad1.left_stick_x, -1, 1);
         Ly = Range.clip(robot.gamepad1.left_stick_y, -1, 1);
-        angle = ((Math.atan2(Ly, Lx) * (180 / Math.PI));
-        positivePower = Math.cos(angle);
-        negativePower = Math.sin(angle);
+        // math to convert the joystick position to an angle value
+        angle = ((Math.atan2(Ly, Lx) * (180 / Math.PI)) + directionFix);
+        // math to determine the power the motors neet to run at
+        positivePower = Math.cos(angle) * lTrig;
+        negativePower = Math.sin(angle) * lTrig;
+        // set the power for the motors
         robot.motorFL.setPower(positivePower);
         robot.motorBR.setPower(positivePower);
         robot.motorFR.setPower(negativePower);
         robot.motorBL.setPower(negativePower);
-
-
-
    }
 }
