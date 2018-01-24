d5820d3a0c9fd736b7fd2d70af15cea9514124fa
MJackBarnes <31902951+MJackBarnes@users.noreply.github.com>
2018-01-24 09:46:28 -0500

    Update HoloX_Drive_Functions.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HoloX_Drive_Functions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HoloX_Drive_Functions.java
@@ -11,86 +11,48 @@ import com.qualcomm.robotcore.util.Range;
 public class HoloX{
 
   public triggerRead (String trigger, int min, int max){
-  
     if (tirgger = "left"){
-    
-        return Range.clip(robot.gamepad1.left_trigger, min, max);
-        
+        return Range.clip(robot.gamepad1.left_trigger, min, max); 
     }
-    
     if (trigger = "right"){
-    
         return Range.clip(robot.gamepad1.right_trigger, min, max);
-        
     }
-    
     else{
-    
         return null;
-        
     }
-    
   }
   
   public stickRead (String stick, String value, int min, int max,){
-  
     if (stick = "rignt" || stick = "Right"){
-    
         if (value = "x" || value = "X"){
-        
             return Range.clip(robot.gamepad1.right_stick_x, min, max);
-            
         }
-        
         else if (value = "y" || value = "Y"){
-        
             return Range.clip(robot.gamepad1.right_stick_y, min, max);
-            
         }
-        
         else{
-        
             return null;
-            
         }
-        
     }
-    
     else if (stick = "left" || stick = "Left") {
-    
         if (value = "x" || value = "X"){
-        
             return Range.clip(robot.gamepad1.left_stick_x, min, max);
-            
         }
-        
         else if (value = "y" || value = "Y"){
-        
             return Range.clip(robot.gamepad1.left_stick_y, min, max);
-            
         }
-        
         else{
-        
             return null;
-            
         }
-    
     }
-    
     else{
-    
-        return null;
-        
+        return null; 
     }
-    
   }
   
   public getStickAngle(String stick){
-  
         return (Math.atan2(stickRead(stick, x, -1, 1), stickRead(stick, y, -1, 1) * (180 / Math.PI));
-        
   }
   
-  public
+  
 }
